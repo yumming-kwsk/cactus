64bb9eb83b791567531a68beebd60573b88f6c70
yumming-kwsk <158579168+yumming-kwsk@users.noreply.github.com>
2025-07-25 11:56:09 +0900

    Update main.js

--- main.js
+++ main.js
@@ -336,8 +336,8 @@ function drawStar(x, y, r, prickleNum) {
 
   function handleTouch(x, y) {
     for (let f of flowers) {
-      let d = dist(x, y, width / 2 + f.x, height / 1.7+100 + f.y);
-      if (d < sabos / 2) {
+      let d = dist(x, y, width / 2 + f.x, height / 1.7 + f.y);
+      if (d < sabos / 1.8) {
         f.isGrowing = true;
       }
     }
